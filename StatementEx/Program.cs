4a8605d9902b2531fa592e97b3e3e528fb30b73e
shwetasapkal123 <sapkalshweta30797@gmail.com>
2022-08-04 08:47:27 +0530

    UC1 check Employee present or absent

--- StatementEx/Program.cs
+++ StatementEx/Program.cs
@@ -6,6 +6,7 @@ namespace StatementEx
     {
         static void Main(string[] args)
         {
+
             //Console.WriteLine("Hello World!");
             //int x = 5;
 
@@ -17,19 +18,31 @@ namespace StatementEx
             //{
             //    Console.WriteLine("x is lesser");
             //}
-            Console.WriteLine("Enter your user name");
-            string userName=Console.ReadLine();
+            //Console.WriteLine("Enter your user name");
+            //string userName=Console.ReadLine();
 
-            Console.WriteLine("Enter password: ");
-            string password=Console.ReadLine();
+            //Console.WriteLine("Enter password: ");
+            //string password=Console.ReadLine();
 
-            if(userName=="Adil" && password=="Adil123")
+            //if(userName=="Adil" && password=="Adil123")
+            //{
+            //    Console.WriteLine("Login successful");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Incorrect userName or password");
+            //}
+            //int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck=random.Next(0,2);
+            Console.WriteLine(empCheck);
+            if(empCheck == 1)
             {
-                Console.WriteLine("Login successful");
+                Console.WriteLine("Employee is present");
             }
             else
             {
-                Console.WriteLine("Incorrect userName or password");
+                Console.WriteLine("Employee is absent");
             }
         }
     }
